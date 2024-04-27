9ccf42d85c32be17216cc2ab39039b69877c1768
compilingcodes <sahil09012004@gmail.com>
2024-04-27 15:56:31 +0530

    Made Page 4

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// Locomotive Smooth Scroll Js Code
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+// Making floating Image using Java Script(JS)
+
+var a=document.querySelector("#elem-container");
+var b=document.querySelector("#fixed-image");
+
+a.addEventListener("mouseenter",function(){
+    b.style.display= "block";
+})
+a.addEventListener("mouseleave",function(){
+    b.style.display= "none";
+})
+
+
+var elems=a.querySelectorAll(".elem");
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image=e.getAttribute("data-image")
+       fixed.style.backgroundImage=`url(${image})`;
+    })
+})
+
